5ee757cd00511a42c3935fb113baef288b918d34
kuberap <47250449+kuberap@users.noreply.github.com>
2023-02-21 11:10:32 +0100

    Update main-interpolace.py

--- main-interpolace.py
+++ main-interpolace.py
@@ -13,6 +13,19 @@ def naive_interp(x,y):
         tmp=tmp*x
     return la.solve(A,y) # vrat vzsledek reseni soustavy rovnic
 
+def lagrange_interp(x,y,xg):
+    assert len(x) == len(y), "Vstupy maji rozdilnou velikost"
+    n = len(x) # pocet vstupnich bodu
+    pocet_bodu = len(xg) # pocet bodu pro ktere bude polynom vyhodnocen
+    yg = np.zeros(pocet_bodu) # sem jsou ukladany spoctene body yg = Ln(xg)
+    for k in range(pocet_bodu):
+        for i in range(n):
+            produkt = 1 # slouzi pro ulozeni soucinu
+            for j in range(n):
+                if j!=i:
+                    produkt*=(xg[k]-x[j])/(x[i]-x[j]) # delej soucin
+            yg[k]+=y[i]*produkt # delej sumaci
+    return yg
 
 if __name__ == "__main__":
     x = np.array([1,2,3,4,5,6])
@@ -23,10 +36,14 @@ if __name__ == "__main__":
 
     x_graf = np.linspace(min(x), max(x),200)
     y_graf = np.polyval(a,x_graf)
+    y_graf_lagrange = lagrange_interp(x,y,x_graf)
 
     plt.plot(x_graf, y_graf,label= "nalezeny polynom")
+    plt.plot(x_graf, y_graf_lagrange, label="Lagrangeuv interpolacni polynom")
     plt.scatter(x,y,label="body vstupu", color = "red")
     plt.grid()
     plt.legend()
     plt.show()
+
+
 
