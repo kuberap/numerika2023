68c81cf41b706bceac86ee790f3e26fe82f154d1
kuberap <47250449+kuberap@users.noreply.github.com>
2023-03-21 10:56:19 +0100

    Add files via upload

---
+++ licho.py
@@ -0,0 +1,32 @@
+import numpy as np
+import  math
+
+
+def integrace_licho(f, a, b, maxiter=20, EPS=1e-4):
+    h=b-a
+    Ih = 0
+    for i in range(1,maxiter):
+        x=np.arange(a,b+h/2,h)
+        fx = f(x)
+        Ih_new = h/2*(fx[0]+2*np.sum(fx[1:-1])+fx[-1])
+        # print(f"x={x}")
+        # print(f"f(x)={x}")
+        print(f"{i}:{h}-->{Ih_new}")
+        # print("*"*20)
+        h = h / 2 # rozpul krok
+        if math.fabs(Ih_new-Ih)< EPS:
+            return Ih_new
+        Ih = Ih_new
+
+    return Ih_new
+
+
+
+
+
+if __name__=="__main__":
+    fce=lambda x:np.cos(x**2)**2
+    a=0
+    b=math.pi/2
+
+    I = integrace_licho(fce,a,b)
