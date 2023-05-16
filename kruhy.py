aa00863ebe9676fc59874cb22822f0dd043af536
kuberap <47250449+kuberap@users.noreply.github.com>
2023-05-16 12:01:20 +0200

    Add files via upload

--- kruhy.py
+++ kruhy.py
@@ -2,12 +2,23 @@ import  numpy as np
 import matplotlib.pyplot as plt
 
 n = 5  # rozmer testovaci matice
-A = 10*np.random.random((n, n))
+A = 100*np.random.random((n, n))
 w,v = np.linalg.eig(A)
+print(w)
 re = [w.real for l in w]
 im = [w.imag for l in w]
 
+centres = [ A[i,i] for i in range(n)]
+radius = [ np.sum(np.abs(A[i,:]))-A[i,i] for i in range(n)]
+
+fig, ax = plt.subplots()
 plt.scatter(x=re, y=im, color="red")
+for c,r in zip(centres, radius):
+
+    ax.add_patch(plt.Circle((c,0), radius=r, color="pink"))
+plt.scatter(x=re, y=im, color="red")
+#plt.scatter(x=centres, y=np.zeros(n), color="blue")
+
 plt.grid()
 plt.show()
 
